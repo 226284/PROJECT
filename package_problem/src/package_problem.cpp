712be51f544087cf05f51f45d49680a33423ee27
226284 <226284@student.pwr.edu.pl>
2017-05-28 12:52:47 +0200

    First commit

---
+++ package_problem/src/package_problem.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : package_problem.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Package problem" << endl; // prints Package problem
+	return 0;
+}
